db2d0bd9e89506aed8a5bd2fc0e81aed66115d47
Muhammad Arif Faizin <muhammadariffaizin@gmail.com>
2022-11-08 14:21:08 +0700

    feat: add localStorage backup for Localization and ColorScheme

--- src/providers/ColorSchemeProvider.js
+++ src/providers/ColorSchemeProvider.js
@@ -1,16 +1,15 @@
 import { useEffect, useState } from "react";
 import ColorSchemeContext from "../contexts/colorScheme";
+import PropTypes from "prop-types";
 
 const ColorSchemeProvider = ({ children }) => {
   const getInitialColorScheme = () => {
     if (typeof window !== "undefined") {
       const userMedia = window.matchMedia("(prefers-color-scheme: dark)");
-
       if (userMedia.matches) {
         return "dark";
       }
     }
-
     return "light";
   };
   const [colorScheme, setColorScheme] = useState(getInitialColorScheme());
@@ -26,7 +25,18 @@ const ColorSchemeProvider = ({ children }) => {
   };
 
   useEffect(() => {
+    const savedColorSchemeState = localStorage.getItem("colorScheme") || null;
+    if (savedColorSchemeState) {
+      setColorScheme(savedColorSchemeState);
+    }
+  }, []);
+
+  useEffect(() => {
     rawSetColorScheme(colorScheme);
+  }, [colorScheme]);
+
+  useEffect(() => {
+    localStorage.setItem("colorScheme", colorScheme);
   }, [colorScheme]);
 
   return (
@@ -34,6 +44,10 @@ const ColorSchemeProvider = ({ children }) => {
       {children}
     </ColorSchemeContext.Provider>
   );
+};
+
+ColorSchemeProvider.propTypes = {
+  children: PropTypes.node.isRequired,
 };
 
 export default ColorSchemeProvider;
--- src/providers/LocaleProvider.js
+++ src/providers/LocaleProvider.js
@@ -1,14 +1,30 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import LanguageContext from "../contexts/language";
+import PropTypes from "prop-types";
 
 const LanguageProvider = ({ children }) => {
   const [language, setLanguage] = useState("id");
+
+  useEffect(() => {
+    const savedLanguageState = localStorage.getItem("language") || null;
+    if (savedLanguageState) {
+      setLanguage(savedLanguageState);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("language", language);
+  }, [language]);
 
   return (
     <LanguageContext.Provider value={{ language, setLanguage }}>
       {children}
     </LanguageContext.Provider>
   );
+};
+
+LanguageProvider.propTypes = {
+  children: PropTypes.node.isRequired,
 };
 
 export default LanguageProvider;
